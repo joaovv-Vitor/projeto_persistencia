466cc43649becf69b72ea22dfed8cdad209f4f6c
joaovv-Vitor <jrodriguescontatato@gmail.com>
2024-11-20 10:17:33 -0300

    alguns ajutes

--- routers/compactacao.py
+++ routers/compactacao.py
@@ -9,11 +9,12 @@ import os
 
 router = APIRouter(prefix='/compactacao',tags=['compactacao'])
 
-csv_file = os.path.join("csv", "publicacoes.csv")
+csv_file = os.path.join('csv', 'publicacoes.csv')
+zip_file = os.path.join('csv', 'publicacoes.zip')
 
 @router.get('/csv_to_zip/', status_code=HTTPStatus.OK)
 def csv_to_zip():
-    with zipfile.ZipFile('publicacoes.zip', 'w', zipfile.ZIP_DEFLATED) as zip:
+    with zipfile.ZipFile(zip_file, 'w', zipfile.ZIP_DEFLATED) as zip:
         zip.write(csv_file, arcname='publicacoes.csv')
     
     return JSONResponse(content={"message": "Arquivo CSV compactado com sucesso."})
